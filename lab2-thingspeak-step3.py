37e654fd29ee165c47d7c23a934b315563a6a6a0
nourjehanfaris <71287903+nourjehanfaris@users.noreply.github.com>
2020-10-01 16:04:25 -0400

    Revert "Master"

---
+++ lab2-thingspeak-step3.py
@@ -0,0 +1,19 @@
+import urllib.request
+import requests
+import threading
+import json
+import random
+
+def thingspeak_post():
+    threading.Timer(15,thingspeak_post).start()
+    val=random.randint(1,30)
+    URl='https://api.thingspeak.com/update?api_key='
+    KEY='XW990P25UBWH2RI9'
+    HEADER='&results=2'.format(val,val)
+    NEW_URL = URl+KEY+HEADER
+    print(NEW_URL)
+    data=urllib.request.urlopen(NEW_URL)
+    print(data)
+    
+if __name__ == '__main__':
+    thingspeak_post() 
